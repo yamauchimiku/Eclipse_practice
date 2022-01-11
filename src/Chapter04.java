32cf53a2f29daadbcb58bf0429d89d99ea5c6320
yamauchimiku <yamamochi7171@gmail.com>
2022-01-11 17:59:48 +0900

    [finish] 1-4

---
+++ src/Chapter04.java
@@ -0,0 +1,17 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		//自分の名前を変数に代入して出力
+		String str = "miku";
+		System.out.println(str);
+		
+		//好きな言葉を同じ変数に代入して出力
+		str = "Hello";
+		System.out.println(str);
+		
+		//ラッキーナンバーとして「int」型で宣言した定数を出力
+		final int LUCKY_NUMBER = 7;
+		System.out.println(LUCKY_NUMBER);
+	}
+
+}
