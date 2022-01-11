d6920cc26cc341e788863a45fd992c8e228b0174
yamauchimiku <yamamochi7171@gmail.com>
2022-01-11 17:12:54 +0900

    [add] 1-4

---
+++ src/Variable01.java
@@ -0,0 +1,19 @@
+
+public class Variable01 {
+
+	public static void main (String[] args) {
+		//メゾットの処理を記述
+		int num1; //int型の変数num1を宣言
+		String str1; //String型の変数str1を宣言
+		num1 = 10; //num1を初期化(宣言した変数に初期値を代入すること)
+		str1 = "Hello World!"; //str1を初期化
+		System.out.println(num1);
+		System.out.println(str1);
+		
+		num1 = 20; //num1に20を再代入
+		System.out.println(num1);
+		
+		int num2 = 30; //int型の変数num2を宣言と同時に初期化
+		System.out.println(num2);
+	}
+}
