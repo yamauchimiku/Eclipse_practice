776375ff2dc20bde5a3b46b3ac05537c2f7cae39
yamauchimiku <yamamochi7171@gmail.com>
2022-01-14 17:57:14 +0900

    [finish] 1-5

--- src/Chapter05.java
+++ src/Chapter05.java
@@ -1,4 +1,41 @@
 
 public class Chapter05 {
+	public static void main(String[] args) {
+		//プリミティブ型
+		
+		long l = 10000000L;
+		//最後にLをつけた整数値で初期化
+	    System.out.println(l);
+	    
+	    //少数値で初期化
+	    double d = 1.12;
+	    System.out.println(d);
+	    
+	    boolean b = true;
+	    System.out.println(b);
+	    
+	    //ラッパークラス型
+	    
+	    //intの値で初期化
+	    Integer i = 100;
+	    System.out.println(i);
+	    
+	    //最後にfを付ける
+	    Float f = 1.123f;
+	    System.out.println(f);
+	    
+	    //''で囲まれた一文字で初期化
+	    Character c = 'あ';
+	    System.out.println(c);
+	    
+	    //int型の配列を初期化してインデックス番号が3の値を出力
+	    int[] array = {0,10,20,30,40};
+	    System.out.println(array[3]);
+	    
+	    
+	    
+	    
+	    
+	}
 
 }
