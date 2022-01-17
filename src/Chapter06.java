0bc25ffe7294a3f900cc8af16ac5d772caf86a44
yamauchimiku <yamamochi7171@gmail.com>
2022-01-17 11:50:43 +0900

    [add] 1-6

--- src/Chapter06.java
+++ src/Chapter06.java
@@ -1,4 +1,14 @@
 
 public class Chapter06 {
+	public static void main(String[] args) {
+		//2足す5
+		int val = 2 + 5;
+        System.out.println(val);
+        
+        //インクリメントしした変数
+        val++;
+        System.out.println(val);
+
+	}
 
 }
